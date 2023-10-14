ba2d7a05ed6bf24f37fc2c169ecc1817d9a75792
Julian Noziglia <julian.noziglia@gmail.com>
2023-10-14 14:04:49 -0300

    getting all the planets together first approach. added pagination template

---
+++ src/components/Pagination.vue
@@ -0,0 +1,35 @@
+<script setup>
+    import { ref } from 'vue';
+    const props =  defineProps({
+        total: {
+            type: Number,
+            required: true
+        },
+        resultsPerPage: {
+            type: Number,
+            required: true
+        }
+    });
+    const emit = defineEmits(['updateCurrent']);
+    const current = ref(1);
+
+    const prev = () => {
+        if (current.value > 1) {
+            current.value--;
+            emit('updateCurrent', current.value);
+        }
+    }
+
+    const next = () => {
+        if (current.value < props.total) {
+            current.value++;
+            emit('updateCurrent', current.value);
+        }
+    }
+</script>
+
+<template>
+    <div>{{ current }} / {{ total }}</div>
+    <div @click="prev">Prev</div>
+    <div @click="next">Next</div>
+</template>
--- src/components/PlanetsList.vue
+++ src/components/PlanetsList.vue
@@ -1,16 +1,67 @@
 <script setup>
+    import Pagination from './Pagination.vue'
     import { ref } from 'vue';
+    let allPlanets = [];
     const planets = ref(null);
-    fetch('https://swapi.dev/api/planets')
-        .then(response => response.json())
-        .then(data => planets.value = data);
-</script>
+    const loading = ref(false);
+    const planetCount = ref(0);
+    const resultsPerPage = ref(10);
+    
+    loading.value = true;
 
-<template>
-    <div v-if="planets">
-        <h5>Planets</h5>
-        <ul class="mb-0">
-            <li v-for="planet in planets.results" :key="planet.id">{{planet.name}}</li>
-        </ul>
-    </div>
-</template>
+    allPlanets = getPlanets('https://swapi.dev/api/planets', []);
+    console.log(allPlanets);
+    loading.value = false;
+    planetCount.value = allPlanets.length;
+    planets.value = allPlanets;
+
+    function getPlanets(url, planetList) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.next) {
+                    return getPlanets(data.next, planetList.concat(data.results));
+                }
+                else {
+                    return planetList.concat(data.results);
+                }
+            });
+    }
+
+    const sortPlanets = () => {
+        planets.value.results.sort((a, b) => {
+            return a.name.localeCompare(b.name);
+        });
+    }
+
+    const updateList = (page) => {
+        console.log(page);
+        loading.value = true;
+        fetch(`https://swapi.dev/api/planets/?page=${page}`)
+            .then(response => response.json())
+            .then(data => {
+                planets.value = data;
+                loading.value = false;
+            });
+    }
+    </script>
+
+    <template>
+        <div>
+            <button role="button" @click="sortPlanets">Sort</button>
+        </div>
+        <div v-if="loading">
+            Loading...
+        </div>
+        <div v-else-if="planets">
+            <h5>Planets</h5>
+            <ul class="mb-0">
+                <li v-for="planet in planets" :key="planet.name">{{planet.name}}</li>
+            </ul>
+        </div>
+        <div>
+            <Pagination v-if="allPlanets.count" :total="Math.ceil(allPlanets.count/resultsPerPage)" :resultsPerPage="resultsPerPage" @updateCurrent="updateList" />
+        </div>
+        <div>Page</div>
+    </template>
+
